6ab97f72cfdcbdb0b21d519242d4da2cf6f032d3
Roman <roomen817@gmail.com>
2024-01-19 00:23:52 +0100

    com

--- src/components/UI/topicModal/TopicModal.jsx
+++ src/components/UI/topicModal/TopicModal.jsx
@@ -21,10 +21,8 @@ const TopicModal = ({ visible, setVisible, createTopic }) => {
 
     if(topic.title.trim() === ''){
       alert("Title should not be empty")
-    } else if (topic.description.length() > 500){
-      alert("Description should be less then 500 characters")
-    } else if (topic.title.length() > 200){
-      alert("Description should be less then 200 characters")
+    } else if (topic.title.length > 200){
+      alert("Title should be less then 200 characters")
     }else{
       const blob = new Blob([JSON.stringify(topic)], {
       type: 'application/json'
